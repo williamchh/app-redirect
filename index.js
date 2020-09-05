ded4cb855b7e67a6985980db9ee2471ab9ddbb3f
ChenHao <williamchh@gmail.com>
2020-09-05 16:51:28 +0930

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const port = 5000;
+
+app.use("/", (req, res) => {
+  res.send(req.headers);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
